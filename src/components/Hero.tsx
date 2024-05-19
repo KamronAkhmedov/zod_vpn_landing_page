b4f5513c837dee34632001a4ae8c5aff95aa792e
coma <coma0705@outlook.com>
2024-05-19 11:28:57 +0500

    test

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,7 +20,7 @@ const Hero = () => {
           <div className=' flex flex-col gap-8 justify-center items-start row-start-2 sm:row-start-1'>
             <h1 className='text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal'>
               Want anything to be easy with {' '}
-              <span className='font-bold'>ZodVPN</span>
+              <span className='font-bold'>Kamrons VPN</span>
             </h1>
             <p className='text-gray-400 mt-4 mb-6'>
               Provide a network for all your needs with ease and  fun using VPN Zod discover interesting features from us
